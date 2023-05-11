8504a9e17aaf3c40aa44fe39477efd4eb8a9a2bf
endless-night <2572745961@qq.com>
2023-05-11 20:20:42 +0800

    Initial commit

---
+++ src/test/java/com/qiyu/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiyu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
